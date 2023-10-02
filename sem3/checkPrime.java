abfb2a38d2d44150121637ca4b45d4f5ece84868
viansebastian <viansbromokusumo@gmail.com>
2023-10-02 21:17:16 +0700

    Update checkPrime.java

--- sem3/checkPrime.java
+++ sem3/checkPrime.java
@@ -1,10 +1,30 @@
 package sem3; 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class checkPrime{
-    public static void A(int n){
-        
+        public static void A(int start, int end, List<Integer> primes) {
+        for (int n = start; n <= end; n++) {
+            boolean isPrime = true;
+            for (int prime : primes) {
+                if (n % prime == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                primes.add(n);
+                System.out.println(n + " is a prime number.");
+            }
+        }
     }
+
     public static void main(String[] args) {
-        
+         List<Integer> primes = new ArrayList<>();
+        primes.add(2);
+        primes.add(3);
+
+        A(4, 1000, primes);
     }
 }
